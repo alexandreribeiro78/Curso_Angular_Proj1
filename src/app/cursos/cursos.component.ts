b3cd8c445901985d2a8c91f286daa4f2cf21e7a2
alexandreribeiro78 <alexandreribeiro78@gmail.com>
2022-07-01 20:14:16 -0300

    Aula 7, Módulo 1 - Curso Angular Loiane

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
+
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
   cursos: string[];
 
-  constructor() { 
+  constructor(private cursosService: CursosService) { 
     this.nomePortal = 'http://loiane.training';
-    this.cursos = ['Java', 'Ext JS', 'Angular'];
+    // this.cursos = [];
+
+    // for (let i=0; i<this.cursos.length; i++){
+    //   let curso = this.cursos[i];
+    // }
+
+    // var servico = new CursosService();
+
+    this.cursos = this.cursosService.getCursos();
+
   }
 
   ngOnInit(): void {
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,10 +1,9 @@
+import { CursosService } from './cursos.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-
-
 
 @NgModule({
   declarations: [
@@ -17,5 +16,8 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
   exports:[
     CursosComponent
   ],
+  providers: [ 
+    CursosService 
+  ]
 })
 export class CursosModule { }
